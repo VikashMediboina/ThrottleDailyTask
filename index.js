acec24a6134d266bb7f4879d12e89f9967fa721d
VikashMediboina <vikash.mediboina@gmail.com>
2020-07-31 11:25:42 +0530

    done with coding UI deploying ...

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ app.get("/personsData", (req, res) => {
 app.get("*", (req, res) => {
     res.send(express.static(path.join(__dirname, './active_person_react/build/index.html')))
 })
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port)
